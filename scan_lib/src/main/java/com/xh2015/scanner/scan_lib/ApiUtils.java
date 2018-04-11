34a2df69bd416544f6353bb43bf8024df2750262
Gary.xu <gary.xu@facilityone.cn>
2018-04-11 09:57:42 +0800

    添加jitpack

---
+++ scan_lib/src/main/java/com/xh2015/scanner/scan_lib/ApiUtils.java
@@ -0,0 +1,18 @@
+package com.xh2015.scanner.scan_lib;
+
+import android.util.Log;
+
+/**
+ * Author：gary
+ * Email: xuhaozv@163.com
+ * description:
+ * Date: 2018/4/11 上午9:39
+ */
+public class ApiUtils {
+
+    private static final String TAG = "ApiUtils";
+
+    public static void printName(String name) {
+        Log.e(TAG, "您输入的姓名为:" + name);
+    }
+}
